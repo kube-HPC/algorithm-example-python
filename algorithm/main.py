48aa5150589255a624eea1cd2b0cba28f34ab3b7
Yehiyam Livneh <yehiyam@gmail.com>
2019-08-04 10:26:26 +0300

    initial

---
+++ algorithm/main.py
@@ -0,0 +1,4 @@
+import time
+def start(args):
+    time.sleep(1)
+    return 42
---
+++ app.py
@@ -0,0 +1,12 @@
+import config
+from hkube_python_wrapper import Algorunner
+
+def main():
+    print("starting algorithm runner")
+    conf = config.Config
+    alg = Algorunner()
+    alg.loadAlgorithm(conf.algorithm)
+    alg.connectToWorker(conf.socket)
+
+if __name__ == "__main__":
+    main()
