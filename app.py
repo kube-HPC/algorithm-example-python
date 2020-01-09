748ec187ca993484df08c24dbeda1c8cabf5aa5b
Yehiyam Livneh <yehiyam@gmail.com>
2020-01-09 09:10:37 +0200

    use threads

--- app.py
+++ app.py
@@ -1,15 +1,19 @@
 import os
+import logging
+import sys
 from hkube_python_wrapper import Algorunner
 from config import Config
 
 import algorithm.main as algorithm
 
 def main():
-    print("starting algorithm runner")
+    logging.info("starting algorithm runner")
     alg = Algorunner()
     alg.loadAlgorithmCallbacks(algorithm.start)
-    job = alg.connectToWorker(Config.socket)
-    job.join()
+    alg.connectToWorker(Config.socket)
+    alg.run()
 
 if __name__ == "__main__":
+    logging.basicConfig(level=0,format='%(relativeCreated)6d %(threadName)s %(message)s', stream=sys.stdout)
+
     main()
