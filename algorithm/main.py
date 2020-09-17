9a44164f2f0d66e40e9c34a59c9b46ba10e1ea93
yehiyam <yehiyam@users.noreply.github.com>
2020-09-17 13:26:17 +0300

    Update main.py

--- algorithm/main.py
+++ algorithm/main.py
@@ -2,7 +2,7 @@ import time
 def start(args, hkubeapi):
     input=args['input']
     if input and input[0] == 'eval-alg':
-        ret = hkubeapi.start_algorithm('eval-alg', [5, 6], resultAsRaw=True, blocking=True)
+        ret = hkubeapi.start_algorithm('eval-alg', [5, 6], resultAsRaw=True)
         time.sleep(2)    
         return ret.get('response') 
     time.sleep(1)
