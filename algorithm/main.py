23a638b25d665d65958dd99683a30399215bd77b
yehiyam <yehiyam@users.noreply.github.com>
2021-08-16 12:08:17 +0300

    add logs in algorithm

--- algorithm/main.py
+++ algorithm/main.py
@@ -1,9 +1,11 @@
 import time
 def start(args, hkubeapi):
+    print('Algorithm start called')
+    print('jobId: {0}, taskId: {1}'.format(args.get('jobId'), args.get('taskId')))
     input=args['input']
     if input and input[0] == 'eval-alg':
         ret = hkubeapi.start_algorithm('eval-alg', [5, 6], resultAsRaw=True)
-        time.sleep(2)    
+        time.sleep(2)
         return ret.get('response') 
     time.sleep(1)
     return 42
